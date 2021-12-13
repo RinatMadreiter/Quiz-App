c293f0de54016ee9cffdf707df3d502d3cdfd815
RinatMadreiter <umfragmail@gmx.de>
2021-12-13 23:08:48 +0100

    added EndGame Screen & Animations and Sound shortened functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /**
- * Json with questions, each question consists of 5 strings and one integer
+ * Json filled with questions, each question consists of 5 strings and one number of the right answer
  */
 
 let questions = [
@@ -61,23 +61,42 @@ let questions = [
     }
 ];
 
+
+/**
+ * define variables: numbers & strings
+ */
 let rightAnsweredQuestions = 0;
 let currentQuestion = 0;
-let Audio_Success = new Audio('./audio/success.mp3');
-Audio_Success.volume = 0.2; 
-let Audio_Failure = new Audio('./audio/failure.mp3');
-Audio_Failure.volume = 0.1; 
+let currentAmountOfQuestions = currentQuestion + 1;
+let rightAnswerNumber = 0;
+let rightAnswerText = "";
+let amountOfQuestions = questions.length;
 
-let quizCard = document.getElementsByClassName("quiz-card");
 
+/**
+ * define variables: audios
+ * lower the volume of all audios to 20% and 10%
+ */
+let audioSuccess = new Audio('./audio/success.mp3');
+audioSuccess.volume = 0.2;
+let audioFailure = new Audio('./audio/failure.mp3');
+audioFailure.volume = 0.1;
+let audioEndGame = new Audio('./audio/endgame.mp3');
+audioEndGame.volume = 0.8;
+
+
+/**
+ * initialise quiz at body=onload
+ */
 function init() {
-    let amountOfQuestions = questions.length;
-    document.getElementById('questionsAmount').innerHTML = `${amountOfQuestions}
-    `;
+    displayAmountOfAllQuestions();
     showQuestion();
-
 }
 
+
+/**
+ * Show questions until all questions answered, then showEndScreen
+ */
 function showQuestion() {
     if (gameIsOver()) {
         showEndScreen();
@@ -88,78 +107,212 @@ function showQuestion() {
 }
 
 
+/**
+ * 1) Check if clicked answer is right
+ * 2) actives when any answer clicked 
+ * 3) audio response if wrong or right answer
+ * 4) CSS response if wrong or right answer
+ * 5) update number "rightAnsweredQuestions" for endscreen if right
+ * 6) reset CSS and animation for next question
+ * @param {string} clickedAnswer 
+ */
 function answer(clickedAnswer) {
-    let rightAnswerNumber = questions[currentQuestion].right_answer;
-    let rightAnswerText = "answer_" + rightAnswerNumber;
-    if (rightAnswerSelected(rightAnswerText, clickedAnswer)) {
-        Audio_Success.play();
-        document.getElementById(clickedAnswer).parentNode.classList.add('right-answer-style');
-        setTimeout(removeAnswerStyle, 230, clickedAnswer, rightAnswerText);
-        rightAnsweredQuestions++; //increases the amont of the rightAnsweredQuestions for the endscreen
+    assignString_rightAnswerText_rightAnswerValue();
+    if (rightAnswerSelected(clickedAnswer)) {
+        playSuccesSoundAndCSSFeedback(clickedAnswer);
     } else {
-        Audio_Failure.play();
-        document.getElementById(clickedAnswer).parentNode.classList.add('wrong-answer-style');
-        document.getElementById(rightAnswerText).parentNode.classList.add('right-answer-style');
-        setTimeout(removeAnswerStyle, 230, clickedAnswer, rightAnswerText);
+        PlayFailureSoundAndCSSFeedback(clickedAnswer);
     }
-    displayArrow();
-    document.getElementById('next-button').disabled = false; // enable Button after first try
-    removeOnclickAttributefromAnswers();
-    removeHoverStyle1fromQuizAnswerCard();
-    addHoverStyle2fromQuizAnswerCard();
+    updateCSSandAnimationsAfterClickedAnswer();
 }
- 
+
+
+/**
+ * 1) play audioFailure
+ * 2) update CSS to see the clickedAnswer is wrong and highlight right answer too
+ * @param {string} clickedAnswer 
+ */
+function PlayFailureSoundAndCSSFeedback(clickedAnswer) {
+    audioFailure.play();
+    document.getElementById(clickedAnswer).parentNode.classList.add('wrong-answer-style');
+    document.getElementById(rightAnswerText).parentNode.classList.add('right-answer-style');
+    setTimeout(removeAnswerStyle, 230, clickedAnswer, rightAnswerText);
+}
+
+
+/**
+ * 1) play audioSuccess
+ * 2) update CSS to see the clickedAnswer is right
+ * 3) update number "rightAnsweredQuestions" for endscreen
+ * @param {string} clickedAnswer 
+ */
+function playSuccesSoundAndCSSFeedback(clickedAnswer) {
+    audioSuccess.play();
+    document.getElementById(clickedAnswer).parentNode.classList.add('right-answer-style');
+    setTimeout(removeAnswerStyle, 230, clickedAnswer);
+    rightAnsweredQuestions++; //increases the amont of the rightAnsweredQuestions for the endscreen
+}
+
+
+/**
+ * update general CSS and animations after clicked answer
+ */
+function updateCSSandAnimationsAfterClickedAnswer() {
+    displayArrow();
+    enableButton();
+    removeOnclickAttributefromAnswers();
+    removeHoverStylePointerFromQuizAnswerCard();
+    addHoverStyleNotAllowedToQuizAnswerCard();
+}
+
+
+/**
+ * Assign the string "rightAnswerText" value of RightAnswer
+ */
+function assignString_rightAnswerText_rightAnswerValue() {
+    rightAnswerNumber = questions[currentQuestion].right_answer;
+    rightAnswerText = "answer_" + rightAnswerNumber;
+}
+
+
 /**
  * Function which removes the "wrong-answer-style" & "right-answer-style"
- * @param {*} clickedAnswer  
- * @param {*} rightAnswerText  
+ * @param {string} clickedAnswer  
  */
-function removeAnswerStyle(clickedAnswer, rightAnswerText) {
+function removeAnswerStyle(clickedAnswer) {
     document.getElementById(clickedAnswer).parentNode.classList.remove('right-answer-style');
     document.getElementById(clickedAnswer).parentNode.classList.remove('wrong-answer-style');
     document.getElementById(rightAnswerText).parentNode.classList.remove('right-answer-style');
     document.getElementById(rightAnswerText).parentNode.classList.remove('wrong-answer-style');
 }
 
+
+/**
+ * 1) display next question
+ * 2) update styles
+ * 3) Check if all questions answered, if yes then Endgame
+ */
 function nextQuestion() {
-    currentQuestion++; // die Frage wird  z.Bsp von 0 auf 1 erhöht
-    document.getElementById('next-button').disabled = true; //disable button at the beginning of the next question
-    let currentAmountOfQuestions = currentQuestion + 1;
-    addOnclickAttributefromAnswers();
-    removeHoverStyle2fromQuizAnswerCard();
-    addHoverStyle1fromQuizAnswerCard();
-    document.getElementById('questionCurrentAmount').innerHTML = `${currentAmountOfQuestions}`;
+    incrementCurrentQuestionNumberForFollowingFunctions();
+    disableButton(); //disable button at the beginning of the next question
+    addOnclickAttributeToAnswers();
+    removeHoverStyleNotAllowedFromQuizAnswerCard();
+    addHoverStylePointerToQuizAnswerCard();
+    hideArrow();
+    updateCurrentAmountOfQuestions();
     showQuestion();
 }
 
 
+/**
+ * restart Game and reset everything
+ */
 function restartGame() {
-    rightAnsweredQuestions = 0;
-    currentQuestion = 0;
-    currentAmountOfQuestions = 1;
+    replaceHeaderImgAndRemoveAnimation();
+    resetNumbers_1rightAnsweredQuestions_2currentQuestion_3currentAmountOfQuestions();
     init();
-    document.getElementById('endScreen').style = 'display: none'; //End-screen verstecken
-    document.getElementById('questionBody').style = ''; // Question Body wieder anzeigen
-    document.getElementById('questionCurrentAmount').innerHTML = `${currentAmountOfQuestions}`;
+    hideEndScreenAndDisplayQuestionBody();
+    updateCurrentAmountOfQuestions();
 }
 
+
+/**
+ * replace headerImg with quizImg and remove animation
+ */
+function replaceHeaderImgAndRemoveAnimation() {
+    document.getElementById('headerImg').classList.remove('w3-animate-zoom');
+    document.getElementById('headerImg').src = './img/quiz.png';
+    document.getElementById('endScreen').classList.remove('w3-animate-zoom');
+}
+
+
+/**
+ * hide endScreen and display questionbody for restartGame function
+ */
+function hideEndScreenAndDisplayQuestionBody() {
+    document.getElementById('endScreen').classList.add('d-none'); //End-screen verstecken
+    document.getElementById('questionBody').style = ''; // Question Body wieder anzeigen
+    document.getElementById('endScreen').classList.remove('w3-animate-zoom');
+    document.getElementById('endScreen').classList.add('d-none'); 
+}
+
+
+/**
+ * Hide Questions and display endscreen-img with zoom in animation 
+ * after endscreen-img load QuizResult with zoom in animation
+ */
 function showEndScreen() {
-    document.getElementById('endScreen').style = ''; //Remove style and it removes Display-none
+    updateEndScreenImage();
+    playEndGameSound();
+    hideQuestionBody();
+    setTimeout(animateQuizResult, 600);
+    displayQuizResult();
+}
+
+
+/**
+ * Play EndGameSound
+ */
+function playEndGameSound() {
+audioEndGame.play();
+}
+
+
+/**
+ * hide questionBody and adjust CSS properties
+ */
+function hideQuestionBody() {
+    
     document.getElementById('questionBody').style = 'display: none';
+    document.getElementById('textQuizEnd').style = 'font-size: 32px; margin-bottom: 10px;';
+    document.getElementById('restartButtonDiv').classList.add('margin-bottom20px');
+}
+
+/**
+ * Display right answered questions
+ */
+function displayQuizResult() {
     document.getElementById('questionsAmountEnd').innerHTML = questions.length;
     document.getElementById('amountOfRightQuestions').innerHTML = rightAnsweredQuestions;
 }
 
-function updateToNextQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('question-text').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+/**
+ * Replace HeaderImage and Credits and add animation to HeaderImage
+ */
+function updateEndScreenImage() {
+    document.getElementById('headerImg').src = './img/trophy.png';
+    document.getElementById('credit').innerHTML = `
+        Illustration by <a href="https://icons8.com/illustrations/author/5c07e68d82bcbc0092519bb6">Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
+        `;
+    document.getElementById('headerImg').classList.add('w3-animate-zoom');
 }
 
 
+/**
+ *  Display quiz result and add zoom-in animation
+ */
+function animateQuizResult() {
+    document.getElementById('endScreen').classList.add('w3-animate-zoom');
+    document.getElementById('endScreen').classList.remove('d-none'); 
+}
+
+
+/**
+ * update innerHTML of Questions
+ */
+function updateToNextQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('question-text').innerHTML = question['question'];
+    for (let index = 1; index <= 4; index++) {
+        document.getElementById(`answer_${index}`).innerHTML = question[`answer_${index}`];
+    }
+}
+
+
+/**
+ * updates updateProgressBar
+ */
 function updateProgressBar() {
     let percent = (currentQuestion + 1) / questions.length * 100;
     document.getElementById('progress-bar').innerHTML = `${percent.toFixed(0)} %`;
@@ -168,69 +321,154 @@ function updateProgressBar() {
 }
 
 
+/**
+ * @returns if Game is over
+ */
 function gameIsOver() {
     return currentQuestion >= questions.length
 }
 
-function rightAnswerSelected(rightAnswerText, clickedAnswer) {
+
+/**
+ * check if strings 1) "clickedAnswer" and 2) "rightAnswerText" are identical 
+ * @param {string} clickedAnswer 
+ * @returns true or false
+ */
+function rightAnswerSelected(clickedAnswer) {
     return rightAnswerText == clickedAnswer
 }
 
+/**
+ * remove onclick attribute from all quiz-answer-cards
+ */
 function removeOnclickAttributefromAnswers() {
-    document.getElementsByClassName("quiz-answer-card")[0].removeAttribute("onclick");
-    document.getElementsByClassName("quiz-answer-card")[1].removeAttribute("onclick");
-    document.getElementsByClassName("quiz-answer-card")[2].removeAttribute("onclick");
-    document.getElementsByClassName("quiz-answer-card")[3].removeAttribute("onclick");
-} 
-
-function addOnclickAttributefromAnswers() {
-    document.getElementsByClassName("quiz-answer-card")[0].setAttribute("onclick", "answer('answer_1')");
-    document.getElementsByClassName("quiz-answer-card")[1].setAttribute("onclick", "answer('answer_2')");
-    document.getElementsByClassName("quiz-answer-card")[2].setAttribute("onclick"," answer('answer_3')");
-    document.getElementsByClassName("quiz-answer-card")[3].setAttribute("onclick", "answer('answer_4')");
-} 
-
-
-function addHoverStyle1fromQuizAnswerCard() {
-    document.getElementsByClassName("quiz-answer-card")[0].classList.add('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[1].classList.add('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[2].classList.add('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[3].classList.add('hover-style1');
-}
-
-function removeHoverStyle1fromQuizAnswerCard() {
-    document.getElementsByClassName("quiz-answer-card")[0].classList.remove('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[1].classList.remove('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[2].classList.remove('hover-style1');
-    document.getElementsByClassName("quiz-answer-card")[3].classList.remove('hover-style1');
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].removeAttribute("onclick");
+    }
 }
 
 
-function addHoverStyle2fromQuizAnswerCard() {
-    document.getElementsByClassName("quiz-answer-card")[0].classList.add('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[1].classList.add('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[2].classList.add('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[3].classList.add('hover-style2');
+/**
+ * add onclick attribute answer_(1-4) to all quiz-answer-cards
+ */
+function addOnclickAttributeToAnswers() {
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].setAttribute("onclick", `answer('answer_${index + 1}')`);
+    }
 }
 
-function removeHoverStyle2fromQuizAnswerCard() {
-    document.getElementsByClassName("quiz-answer-card")[0].classList.remove('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[1].classList.remove('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[2].classList.remove('hover-style2');
-    document.getElementsByClassName("quiz-answer-card")[3].classList.remove('hover-style2');
+
+/**
+ * add hover style pointer to all quiz-answer-cards
+ */
+function addHoverStylePointerToQuizAnswerCard() {
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].classList.add('hover-style-pointer');
+    }
 }
 
+
+/**
+ * remove  hover style pointer from all quiz-answer-cards
+ */
+function removeHoverStylePointerFromQuizAnswerCard() {
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].classList.remove('hover-style-pointer');
+    }
+}
+
+
+/**
+ * add hover style not-allowed to all quiz-answer-cards
+ */
+function addHoverStyleNotAllowedToQuizAnswerCard() {
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].classList.add('hover-style-not-allowed');
+    }
+}
+
+
+/**
+ * remove hover style not-allowed from all quiz-answer-cards
+ */
+function removeHoverStyleNotAllowedFromQuizAnswerCard() {
+    for (let index = 0; index <= 3; index++) {
+        document.getElementsByClassName("quiz-answer-card")[index].classList.remove('hover-style-not-allowed');
+    }
+}
+
+
+/**
+ * diplay animated right-arrow >>
+ */
 function displayArrow() {
     document.getElementById('arrow').classList.remove('d-none');
 }
 
+
+/**
+ * hide animated right-arrow >>
+ */
 function hideArrow() {
     document.getElementById('arrow').classList.add('d-none');
+}
+
+
+/**
+ * enable Button (NextQuestion) after first try
+ */
+function enableButton() {
+    document.getElementById('next-button').disabled = false;
+}
+
+
+/**
+ * disable Button (NextQuestion) after first try
+ */
+function disableButton() {
+    document.getElementById('next-button').disabled = true;
+}
+
+
+/**
+ * Updates the number of current displayed question
+ */
+function updateCurrentAmountOfQuestions() {
+    document.getElementById('questionCurrentAmount').innerHTML = `${currentAmountOfQuestions}`;
+}
+
+
+/**
+ * increment number currentQuestion for following functions
+ */
+function incrementCurrentQuestionNumberForFollowingFunctions() {
+    currentQuestion++; // die Frage wird  z.Bsp von 0 auf 1 erhöht
+    currentAmountOfQuestions++;
+}
+
+
+/**
+ * reset following numbers for restart:
+ * 1) rightAnsweredQuestions
+ * 2) currentQuestion
+ * 3) currentAmountOfQuestions
+ */
+function resetNumbers_1rightAnsweredQuestions_2currentQuestion_3currentAmountOfQuestions() {
+    rightAnsweredQuestions = 0;
+    currentQuestion = 0;
+    currentAmountOfQuestions = 1;
+}
+
+/**
+ * display amount of all questions according to the lenght of ""questions" JSON
+ */
+function displayAmountOfAllQuestions() {
+    document.getElementById('questionsAmount').innerHTML = `${amountOfQuestions}`;
 }
 /**
  * ✔
  * todo: lautstärke anpassen ✔
- * todo: frage nur ein mal beantwortbar erledigt
+ * todo: frage nur ein mal beantwortbar ✔
  * todo show end-screen
  *
  */
